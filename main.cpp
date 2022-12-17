f57b2d980defa593474a7ae7d7f5cd40d55bf30d
joaooliveiraa11 <93929858+joaooliveiraa11@users.noreply.github.com>
2022-12-17 00:48:53 +0000

    Verificação de leitura dos dados

--- main.cpp
+++ main.cpp
@@ -2,9 +2,41 @@
 #include "Classes/Reading.h"
 
 int main() {
-    vector<Airline> airlines;
     Reading reading = Reading();
-
+/*
+    vector<Flight> flights; // Source,Target,Airline
+    flights = reading.readFlights();
+    for(Flight flight : flights){
+        cout << flight.getFlightsource() << endl;
+        cout << flight.getFlighttarget() << endl;
+        cout << flight.getFlightairline() << endl;
+    }
+    ta a dar
+*/
+    /*
+    airports = reading.readAirports();
+    vector<Airport> airports; // Code,Name,City,Country,Latitude,Longitude
+    for(Airport airport : airports){
+        cout << airport.getAirportcode() << endl;
+        cout << airport.getAirportname() << endl;
+        cout << airport.getAirportcity() << endl;
+        cout << airport.getAirportcountry() << endl;
+        cout << airport.getAirportlatitude() << endl;
+        cout << airport.getAirportlongitude() << endl;
+    }
+     está a dar mas o float está a cortar casas decimais e a arredondar
+    */
+    /*
+    airlines = reading.readAirLines();
+    vector<Airline> airlines; // Code,Name,Callsign,Country
+    for(Airline airline : airlines){
+        cout << airline.getAirlinecode() << endl;
+        cout << airline.getAirlinename() << endl;
+        cout << airline.getAirlinecallsign() << endl;
+        cout << airline.getAirlinecountry() << endl;
+    }
+     ta a dar
+     */
 
 
     return 0;
